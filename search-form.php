3a20ef72874d36b64af7753294ac3c91af91691c
damon23 <webeuromatech@gmail.com>
2017-07-17 12:06:30 +0400

    course-search-form.php
    This is the code for the search form. Copy the code and place it anywhere in your theme. Change the name of the taxonomies according to your own.

---
+++ search-form.php
@@ -0,0 +1,46 @@
+<form name="search-fomr" action="<?php echo site_url().'/seminars-list/'; ?>" method="get">
+      <input type="text" name="seminar" placeholder="Search our seminars" class="seminars-input">
+<select name="category" class="category-select">
+       <option value="">Category</option>
+       <?php  
+       $terms = get_terms( 'course_category','hide_empty=0' );
+       if ( ! empty( $terms ) && ! is_wp_error( $terms ) ){
+
+
+           foreach ( $terms as $term ) {
+            $term_link = get_term_link( $term );
+            echo '<option value="' . $term->slug . '"><a href=>' . $term->name . '</option>';
+
+        }
+
+    }
+		 // Code for Downloads
+    ?>
+</select>
+<select name="city" class="category-select">
+   <option value="">City</option>
+   <?php  $terms = get_terms( 'course_venues','hide_empty=0' );
+   if ( ! empty( $terms ) && ! is_wp_error( $terms ) ){
+
+       foreach ( $terms as $term ) {
+           echo '<option value="' . $term->slug . '"><a href=>' . $term->name . '</option>';
+
+    }
+
+}?>
+</select>
+
+<select name="month" class="category-select">
+   <option value="">Month</option>
+   <?php  $terms = get_terms( 'course_month','hide_empty=0' );
+   if ( ! empty( $terms ) && ! is_wp_error( $terms ) ){
+
+       foreach ( $terms as $term ) {
+       echo '<option value="' . $term->slug . '"><a href=>' . $term->name . '</option>';
+
+    }
+
+}?>
+</select>
+<input type="submit" id="search" name="search" value="Search" class="search-btn">
+</form>	
